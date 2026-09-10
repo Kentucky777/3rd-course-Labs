0204cc2788a078f4d92d4e1574d7778c8144492f
Байков Никита <kentucky.twitch@mail.ru>
2026-09-10 17:57:39 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -2,10 +2,27 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/web")
 def start():
     return """<!docktype html>
         <html>
            <body>
                <h1> web-сервер на Flask</h1>
+               <p><a href="/author">Автор</a></p>
            <body>
         <html>"""
+@app.route("/author")
+def author():
+    name = "Байков Никита Дмитриевич"
+    group = "ФБИ-41"
+    faculty = "ФБ"
+
+    return """<!doctype html>
+        <html>
+            <body>
+            <p> Студент: """ + name + """</p>
+            <p> Группа : """ + group + """</p>
+            <p> Факультет: """ + faculty + """
+            <p><a href="/web">web</a></p>
+            </body>
+        </html>"""
